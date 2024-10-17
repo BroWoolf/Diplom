0025a4e16be02b3f4dd09d95559a4886614d21f5
BroWoolf <BroWoolfik@gmail.com>
2024-10-17 15:02:12 +0300

    Исправил автотест

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,7 +4,5 @@ import requests
 def create_order():
     return requests.post(configuration.URL +configuration.CREATE_PATH,
                   json = data.body).json()['track']
-def get_url():
-    return configuration.URL + configuration.GET_ORDER_PATH + str(create_order())
-
-print (get_url())
+def get_order_by_track(track):
+    return requests.get(configuration.URL + configuration.GET_ORDER_PATH + str(track))
--- tests.py
+++ tests.py
@@ -1,10 +1,6 @@
 import sender_stand_request
-import requests
 
 # Виктор Павлов, 22-я когорта - Финальный проект. Инженер по тестированию плюс.
 
-def positive():
-    assert requests.get(sender_stand_request.get_url()).status_code == 200
-
-def test_can_get_body():
-    return positive()
+def test_get_order_by_track_success():
+    assert sender_stand_request.get_order_by_track(sender_stand_request.create_order()).status_code == 200
